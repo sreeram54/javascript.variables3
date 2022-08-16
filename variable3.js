a1699adcc3f8669faf0a1fb11c8e5440644471c3
sreeram54 <vaddisreeram54@gmail.com>
2022-08-16 22:07:49 +0530

    variable3

---
+++ variable3.js
@@ -0,0 +1,5 @@
+var gravity = 9.81 // earth gravity in m/s2
+var boilingpoint= 100 //water boiling point,temperature in oC
+var PI = 3.14 // gemetrical constant 
+ 
+console.log(gravity,boilingpoint,PI)
